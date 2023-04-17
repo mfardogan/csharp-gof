23d3dc3953e7ee13f8225bf97d019f5a4ca8f36e
mfard <mfard@ARDO-GAN>
2023-04-18 02:37:34 +0300

    [dev]: Strategy

--- State/Program.cs
+++ State/Program.cs
@@ -1,187 +1,189 @@
-﻿//namespace State
-//{
-
-abstract class Computer
+﻿namespace State
 {
-    public ComputerState State { get; set; }
-}
 
-
-abstract class ComputerState
-{
-    public abstract void Handle(Computer computer);
-}
-
-
-class MyComputer : Computer
-{
-    public MyComputer()
+    abstract class Computer
     {
-        /* 
-           Başlangıçta bilgisayarın kapalı konumda olduğunu varsayabiliriz.  
-         */
-
-        State = new OffState();
+        public ComputerState State { get; set; }
     }
 
-    public void Power()
+
+    abstract class ComputerState
     {
-        /* Düğmeye basılması anında meydana gelecek davranışın karmaşık
-           detaylarını Power fonksiyonu bilmemelidir! Bunun yerine State
-           sınıflarına başvurmalıdır. Bu durum sisteme ilerleyen zamanlarda
-           entegre edeceğiniz yeni durum sınıflarına programının kolayca adapte
-           olmasını sağlar. Fonksiyon, gelişime açık ve değişime kapalı
-           (Open-Closed) olarak tasarlanmış olur. 
-         */
-
-        State.Handle(this);
+        public abstract void Handle(Computer computer);
     }
-}
 
 
-class OffState : ComputerState
-{
-    public override void Handle(Computer computer)
+    class MyComputer : Computer
     {
-        /*
-           Bilgisayar, kapalı durumdayken düğmeye basılırsa açık duruma geçer.
-         */
-
-        computer.State = new OnState();
-        Console.WriteLine("Bilgisayar açıldı!");
-
-    }
-}
-
-class OnState : ComputerState
-{
-    public override void Handle(Computer computer)
-    {
-        /*
-           Bilgisayar, açık durumdayken düğmeye basılırsa kapalı duruma geçer.
-         */
-
-        computer.State = new OffState();
-        Console.WriteLine("Bilgisayar kapandı!");
-
-    }
-}
-
-
-internal class Program
-{
-    //static void Main(string[] args)
-    //{
-    //    /*
-    //       Aynı Power fonksiyonunun, mevcut durumu kaale alarak
-    //       farklı bir davranışı yerine getirmesini izleyelim. 
-    //     */
-
-    //    MyComputer computer = new MyComputer(); //kapalı durumda
-    //    computer.Power(); //açık konuma geçti
-    //    computer.Power(); //kapalı konuma geçti
-
-    //}
-    ///* ÇIKTI:
-    //   Bilgisayar açıldı!
-    //   Bilgisayar kapandı!
-    // */
-
-    static void Main(string[] args)
-    {
-
-        DriveModeFactory factory = new DriveModeFactory();
-        Car bmw = new Car();
-        bmw.StepOnTheGas();
-
-        bmw.State = factory.Create(Gears.R);
-        bmw.StepOnTheGas();
-
-        bmw.State = factory.Create(Gears.D);
-        bmw.StepOnTheGas();
-
-    }
-    /* ÇIKTI:
-       Araç park modunda.
-       Araç geriye doğru gidiyor.
-       Araç ileriye doğru gidiyor.
-     */
-}
-//}
-
-
-abstract class DriveMode
-{
-    public abstract void Handle(Car car);
-}
-
-class Drive : DriveMode
-{
-    public override void Handle(Car car)
-    {
-        //D modundayken gaza basılırsa araç ilerler.
-        Console.WriteLine("Araç ileriye doğru gidiyor.");
-    }
-}
-
-class Reverse : DriveMode
-{
-    public override void Handle(Car car)
-    {
-        //R modundayken gaza basılırsa araç geri gider.
-        Console.WriteLine("Araç geriye doğru gidiyor.");
-    }
-}
-
-class Park : DriveMode
-{
-    public override void Handle(Car car)
-    {
-        //P modundayken gaza basılırsa araç ilerlemez.
-        Console.WriteLine("Araç park modunda.");
-    }
-}
-
-class Neutral : DriveMode
-{
-    public override void Handle(Car car)
-    {
-        //N modundayken gaza basılırsa araç ilerlemez.
-        Console.WriteLine("Araç boşta.");
-    }
-}
-
-enum Gears
-{
-    P, R, N, D
-}
-
-class DriveModeFactory
-{
-    public DriveMode Create(Gears gears)
-    {   //Bkz. Factory Method (Factory) pattern.
-        switch (gears)
+        public MyComputer()
         {
-            case Gears.P: return new Park();
-            case Gears.R: return new Reverse();
-            case Gears.N: return new Neutral();
-            default: case Gears.D: return new Drive();
+            /* 
+               Başlangıçta bilgisayarın kapalı konumda olduğunu varsayabiliriz.  
+             */
+
+            State = new OffState();
+        }
+
+        public void Power()
+        {
+            /* Düğmeye basılması anında meydana gelecek davranışın karmaşık
+               detaylarını Power fonksiyonu bilmemelidir! Bunun yerine State
+               sınıflarına başvurmalıdır. Bu durum sisteme ilerleyen zamanlarda
+               entegre edeceğiniz yeni durum sınıflarına programının kolayca adapte
+               olmasını sağlar. Fonksiyon, gelişime açık ve değişime kapalı
+               (Open-Closed) olarak tasarlanmış olur. 
+             */
+
+            State.Handle(this);
         }
     }
+
+
+    class OffState : ComputerState
+    {
+        public override void Handle(Computer computer)
+        {
+            /*
+               Bilgisayar, kapalı durumdayken düğmeye basılırsa açık duruma geçer.
+             */
+
+            computer.State = new OnState();
+            Console.WriteLine("Bilgisayar açıldı!");
+
+        }
+    }
+
+    class OnState : ComputerState
+    {
+        public override void Handle(Computer computer)
+        {
+            /*
+               Bilgisayar, açık durumdayken düğmeye basılırsa kapalı duruma geçer.
+             */
+
+            computer.State = new OffState();
+            Console.WriteLine("Bilgisayar kapandı!");
+
+        }
+    }
+
+
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    /*
+        //       Aynı Power fonksiyonunun, mevcut durumu kaale alarak
+        //       farklı bir davranışı yerine getirmesini izleyelim. 
+        //     */
+
+        //    MyComputer computer = new MyComputer(); //kapalı durumda
+        //    computer.Power(); //açık konuma geçti
+        //    computer.Power(); //kapalı konuma geçti
+
+        //}
+        ///* ÇIKTI:
+        //   Bilgisayar açıldı!
+        //   Bilgisayar kapandı!
+        // */
+
+        static void Main(string[] args)
+        {
+
+            DriveModeFactory factory = new DriveModeFactory();
+            Car bmw = new Car();
+            bmw.StepOnTheGas();
+
+            bmw.State = factory.Create(Gears.R);
+            bmw.StepOnTheGas();
+
+            bmw.State = factory.Create(Gears.D);
+            bmw.StepOnTheGas();
+
+        }
+        /* ÇIKTI:
+           Araç park modunda.
+           Araç geriye doğru gidiyor.
+           Araç ileriye doğru gidiyor.
+         */
+    }
+
+    abstract class DriveMode
+    {
+        public abstract void Handle(Car car);
+    }
+
+    class Drive : DriveMode
+    {
+        public override void Handle(Car car)
+        {
+            //D modundayken gaza basılırsa araç ilerler.
+            Console.WriteLine("Araç ileriye doğru gidiyor.");
+        }
+    }
+
+    class Reverse : DriveMode
+    {
+        public override void Handle(Car car)
+        {
+            //R modundayken gaza basılırsa araç geri gider.
+            Console.WriteLine("Araç geriye doğru gidiyor.");
+        }
+    }
+
+    class Park : DriveMode
+    {
+        public override void Handle(Car car)
+        {
+            //P modundayken gaza basılırsa araç ilerlemez.
+            Console.WriteLine("Araç park modunda.");
+        }
+    }
+
+    class Neutral : DriveMode
+    {
+        public override void Handle(Car car)
+        {
+            //N modundayken gaza basılırsa araç ilerlemez.
+            Console.WriteLine("Araç boşta.");
+        }
+    }
+
+    enum Gears
+    {
+        P, R, N, D
+    }
+
+    class DriveModeFactory
+    {
+        public DriveMode Create(Gears gears)
+        {   //Bkz. Factory Method (Factory) pattern.
+            switch (gears)
+            {
+                case Gears.P: return new Park();
+                case Gears.R: return new Reverse();
+                case Gears.N: return new Neutral();
+                default: case Gears.D: return new Drive();
+            }
+        }
+    }
+
+
+    class Car
+    {
+        public Car()
+        {
+            State = new Park();
+        }
+
+        public DriveMode State { get; set; }
+
+        public void StepOnTheGas()
+        {
+            State.Handle(this);
+        }
+    }
+
 }
 
 
-class Car
-{
-    public Car()
-    {
-        State = new Park();
-    }
-
-    public DriveMode State { get; set; }
-
-    public void StepOnTheGas()
-    {
-        State.Handle(this);
-    }
-}
---
+++ Strategy/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Strategy
+{
+    interface ISortStrategy
+    {
+        /*
+           Her şeyden evvel, streteji sınıflarının uyması gereken strandartları
+           anlatan bir abstraction gerekli. Bir sıralama algoritması parametre olarak 
+           bir dizi alıp geriye bir dizi teslim eder.
+         */
+        int[] Execute(int[] numbers);
+    }
+
+
+    class BubbleSort : ISortStrategy
+    {
+        public int[] Execute(int[] numbers)
+        {
+            Console.WriteLine("Dizi, Bubble Sort yöntemiyle sıralandı...");
+            /* Konudan uzaklamaşmamak için sıralama algoritmasının detaylarına 
+              girmiyorum. */
+            return numbers.OrderBy(e => e).ToArray();
+        }
+    }
+
+    class MergeSort : ISortStrategy
+    {
+        public int[] Execute(int[] numbers)
+        {
+            Console.WriteLine("Dizi, Merge Sort yöntemiyle sıralandı...");
+            /* Konudan uzaklamaşmamak için sıralama algoritmasının detaylarına
+               girmiyorum. */
+            return numbers.OrderBy(e => e).ToArray();
+        }
+    }
+
+
+    class SortingStrategy
+    {
+        public SortingStrategy(ISortStrategy strategy)
+        {
+            Strategy = strategy;
+        }
+
+        /*
+           Burada bir composition yapılır. ISortStrategy ara birimini uygulamış tüm 
+           sınıf nesneleri bu alana verilebilir. Üst seviyedeki sınıf, doğrudan alt 
+           seviyedeki sınıfların kendilerine değil, onların uyguladığı ISortStrategy ara 
+           birimine bağımlı olur. Buna Export Coupling adı verilir ve bunun uygulaması 
+           Dependency Inversion sağlar.
+         */
+
+        public ISortStrategy Strategy { get; set; }
+
+        public int[] Execute(int[] numbers)
+        {
+            /*
+              Context sınıfı kendisine parametre olarak verilmiş
+              Strategy nesnesinin Execute fonksiyonunu çağırır.
+              Algoritmanın detaylarına dair hiç bir şey bilmez!
+             */
+            return Strategy.Execute(numbers);
+        }
+    }
+
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            /*
+              Sıralama yöntemini değiştirmek istediğinizde yalnızca kurucu fonksiyon 
+             parametresini aşağıdan değiştirmeniz yeterli olacaktır.
+             */
+            SortingStrategy strategy =
+                 new SortingStrategy(new BubbleSort());
+
+            int[] unsorted = { 1, 5, -3, 2, 90, 12, 9 };
+            int[] sorted1 = strategy.Execute(unsorted);
+
+            /* Dilediğiniz yerde stratejiyi değiştirmeniz mümkün.  */
+
+            strategy.Strategy = new MergeSort();
+            int[] sorted2 = strategy.Execute(unsorted);
+        }
+        /* ÇIKTI:
+         Dizi, Bubble Sort yöntemiyle sıralandı...
+         Dizi, Merge Sort yöntemiyle sıralandı...
+         */
+
+    }
+}
