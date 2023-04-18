4eacf3273c71bd6ac886dd73513be2466a3e9606
mfard <mfard@ARDO-GAN>
2023-04-18 21:05:42 +0300

    [dev]: Factory

---
+++ Builder/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace Builder
+{
+
+    class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public void ShowOff()
+        {
+            Console.WriteLine($"{Color} {Brand} {Model} çalıştı...");
+        }
+    }
+
+
+    interface ICarBuilder
+    {
+        Car Get(); //Hazır hale gelmiş nesneyi döndürür.
+        void BuildBrand(); //Araç markasını ayarlar
+        void BuildModel(); //Araç modelini ayarlar
+        void BuildColor(); //Araç rengini ayarlar
+    }
+
+
+    class BmwBuilder : ICarBuilder
+    {
+        private Car car = new Car();
+
+        public Car Get()
+        {
+            return car;
+        }
+
+        public void BuildBrand()
+        {
+            car.Brand = "Bmw";
+        }
+
+        public void BuildModel()
+        {
+            car.Model = "E38";
+        }
+
+        public void BuildColor()
+        {
+            car.Color = "Kırmızı";
+        }
+    }
+
+
+
+    class MercedesBuilder : ICarBuilder
+    {
+        private Car car = new Car();
+
+        public Car Get()
+        {
+            return car;
+        }
+
+        public void BuildBrand()
+        {
+            car.Brand = "Mercedes";
+        }
+
+        public void BuildModel()
+        {
+            car.Model = "C180";
+        }
+
+        public void BuildColor()
+        {
+            car.Color = "Siyah";
+        }
+    }
+
+
+    class BuilderDirector
+    {
+        public ICarBuilder Builder { get; set; }
+        public BuilderDirector(ICarBuilder builder)
+        {
+            Builder = builder;
+        }
+
+        public Car Build()
+        {
+            /*
+               Director sınıfı, nesnenin kullanıma hazır hale gelmesi için gereken 
+               adımların ne olduğunu ve hangi sırayla uygulanması gerektiğini bilir.
+               Bu detayları istemci tarafından gizlemek için kullanılır.
+             */
+
+            Builder.BuildBrand();
+            Builder.BuildModel();
+            Builder.BuildColor();
+            return Builder.Get();
+        }
+    }
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BuilderDirector director = new BuilderDirector(new BmwBuilder());
+            Car bmw = director.Build();
+            bmw.ShowOff();
+
+            director.Builder = new MercedesBuilder();
+            Car mercedes = director.Build();
+            mercedes.ShowOff();
+        }
+        /* ÇIKTI:
+          Kırmızı Bmw E38 çalıştı...
+          Siyah Mercedes C180 çalıştı...
+         */
+
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,96 @@
+﻿//namespace Factory
+//{
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        VehicleFactory factory = new VehicleFactory();
+        var car = factory.Create(VehicleType.Mercedes);
+        car.Drive();
+
+        /*
+          Alt tip kullanılmak istendiğinde casting işlemi yapılabilir.
+         */
+        Bmw bmw = (Bmw)factory.Create(VehicleType.Bmw);
+        bmw.Drive();
+    }
+    /* ÇIKTI:
+       Mercedes is moving...
+       Bmw is moving...
+     */
+
+}
+
+class Vehicle
+{
+    /*
+       Factory Method, aynı ürün ailesinden meydana gelmiş sınıflardan nesne 
+       oluşturmayı amaçlar. Aynı ürün ailesinden kasıt, aynı temel sınıf veya ara 
+       birimden türemiş sınıflardır. Örneğimiz için ürün ailesi Vehicle isimli bu 
+       sınıfı uygulamış alt sınıflar olacaktır.
+     */
+    public string Type { get; }
+
+    public Vehicle(string type)
+    {
+        Type = type;
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine(Type + " is moving...");
+    }
+}
+
+//Ürün-1
+class Mercedes : Vehicle
+{
+    public Mercedes() : base("Mercedes")
+    {
+    }
+}
+
+//Ürün-2
+class Bmw : Vehicle
+{
+    public Bmw() : base("Bmw")
+    {
+    }
+}
+
+
+enum VehicleType : byte
+{
+    Mercedes,
+    Bmw
+}
+
+class VehicleFactory
+{
+    public Vehicle Create(VehicleType type)
+    {
+        /*
+             "type" parametresinin değerine göre hangi türde
+             nesne üretileceğine karar verilir.
+             Üretilir ve istemci tarafına iletilir.
+         */
+        Vehicle vehicle = null;
+        switch (type)
+        {
+            case VehicleType.Bmw:
+                vehicle = new Bmw();
+                break;
+
+            case VehicleType.Mercedes:
+                vehicle = new Mercedes();
+                break;
+        }
+
+        return vehicle;
+    }
+}
+
+
+
+
+//}
